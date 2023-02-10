488f971ee95bad589764802b7240cb27399063b0
EgorK <koegor06@gmail.com>
2023-02-10 17:21:41 +0300

    Baseline

---
+++ main/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+
+def index(request):
+    # if request.user.is_authenticated:
+    #     pass
+    #
+    # events = Article.objects.filter(div_code=0)
+    #
+    # context = {
+    #     'events': events,
+    #     'news': {},
+    # }
+
+    return render(request, 'index.html', {})
