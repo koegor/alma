2040fefe0c38f749554a46279c494267bbbfae76
EgorK <koegor06@gmail.com>
2023-02-12 14:43:18 +0300

    Pages: index, contacts

--- main/views.py
+++ main/views.py
@@ -13,3 +13,17 @@ def index(request):
     # }
 
     return render(request, 'index.html', {})
+
+
+def contacts(request):
+    # if request.user.is_authenticated:
+    #     pass
+    #
+    # events = Article.objects.filter(div_code=0)
+    #
+    # context = {
+    #     'events': events,
+    #     'news': {},
+    # }
+
+    return render(request, 'contacts.html', {})
