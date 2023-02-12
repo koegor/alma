d4f5c95e3b5d526fd2ebf47c9de0e4895876cd5b
EgorK <koegor06@gmail.com>
2023-02-12 15:41:43 +0300

    Add models and admin pannel

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
+from main.models import Inst, Specialty, Property
+
 # Register your models here.
+
+
+# class SpecialityInline(admin.TabularInline):
+#     model = Specialty
+#
+#
+# class PropertyInline(admin.TabularInline):
+#     model = Property
+#
+#
+# class InstAdmin(admin.ModelAdmin):
+#     inlines = [
+#         SpecialityInline,
+#         PropertyInline
+#     ]
+
+
+admin.site.register(Inst)
+admin.site.register(Specialty)
+admin.site.register(Property)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Specialty(models.Model):
+    name = models.CharField(max_length=200, unique=True, verbose_name='Название направления')
+    code = models.CharField(max_length=10, verbose_name='Код направления')
+    url = models.URLField(verbose_name="Адрес сайта")
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Направление'
+        verbose_name_plural = 'Направления'
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+TYPES = (
+    (0, "Тип Да/Нет"),
+    (1, "Тип число")
+)
+
+
+class Property(models.Model):
+    name = models.CharField(max_length=200, unique=True, verbose_name='Название признака')
+    quest = models.CharField(max_length=200, verbose_name='Вопрос')
+    typ = models.IntegerField(choices=TYPES, unique=True, verbose_name='Вопрос')
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Признак'
+        verbose_name_plural = 'Признаки'
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Inst(models.Model):
+    name = models.CharField(max_length=200, unique=True, verbose_name='Институт')
+    short_name = models.CharField(max_length=10)
+    url = models.URLField(verbose_name="Адрес сайта")
+    specialities = models.ManyToManyField(Specialty)
+    properties = models.ManyToManyField(Property)
+
+    class Meta:
+        ordering = ['short_name']
+        verbose_name = 'Институт'
+        verbose_name_plural = 'Институты'
+
+    def __str__(self):
+        return f"{self.short_name}"
